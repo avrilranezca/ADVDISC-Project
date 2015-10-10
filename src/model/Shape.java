d44a202eebd5fa8a529351a3b2cbfe28c910deb9
piperching <jan_kristoffer96@yahoo.co.uk>
2015-10-10 16:05:40 +0800

    Try commit
    asd

--- src/model/Shape.java
+++ src/model/Shape.java
@@ -6,4 +6,6 @@ public abstract class Shape {
 	public abstract void rotate(double angle);
 	public abstract void reflectOverX();
 	public abstract void reflectOverY();
+	public abstract void rescaleX(double percentage);
+	public abstract void rescaleY(double percentage);
 }
