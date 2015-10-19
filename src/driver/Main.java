f4bbb8d702a4ee23d2bbf04228d36d265c4c60ce
avrilranezca <fernandezavril16@gmail.com>
2015-10-19 21:29:29 +0800

    Started on Parabolas

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -2,13 +2,21 @@ package driver;
 
 import model.GraphicObject;
 import model.Line;
+import model.Parabola;
 import model.Point;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GraphicObject shape = new Line(new Point(1, 3), new Point(4, 2));
+		/*GraphicObject shape = new Line(new Point(1, 3), new Point(4, 2));
 		shape.rescaleX(0.5);
-		System.out.println( ((Line) shape).getTail());
+		System.out.println( ((Line) shape).getTail());*/
+		
+		GraphicObject shape = new Parabola(new Point(2, -1), new Point(2, 1), "x", -3);
+		System.out.println(((Parabola) shape).getVertex());
+		shape.translate(3, 4);
+		System.out.println( ((Parabola) shape).getVertex());
+		System.out.println( ((Parabola) shape).getFocus());
+		System.out.println( ((Parabola) shape).getDirectrixAxis() + "=" + ((Parabola) shape).getDirectrixValue());
 	}
 }
---
+++ src/model/Parabola.java
@@ -0,0 +1,115 @@
+package model;
+
+public class Parabola extends GraphicObject{
+	
+	private Point vertex;
+	private Point focus;
+	private String directrixAxis;
+	private double directrixValue;
+	
+	public Parabola(Point vertex, Point focus, String directrixAxis, double directrixValue) {
+		this.vertex = vertex;
+		this.focus = focus;
+		this.directrixAxis = directrixAxis;
+		this.directrixValue = directrixValue;
+	}
+	
+	public Point getVertex() {
+		return vertex;
+	}
+	
+	public void setVertex(Point vertex) {
+		this.vertex = vertex;
+	}
+	
+	public Point getFocus() {
+		return focus;
+	}
+
+	public void setFocus(Point focus) {
+		this.focus = focus;
+	}
+
+	public String getDirectrixAxis() {
+		return directrixAxis;
+	}
+
+	public void setDirectrixAxis(String directrixAxis) {
+		this.directrixAxis = directrixAxis;
+	}
+
+	public double getDirectrixValue() {
+		return directrixValue;
+	}
+
+	public void setDirectrixValue(double directrixValue) {
+		this.directrixValue = directrixValue;
+	}
+
+	
+	@Override
+	public void translate(int height, int width) {
+		// TODO Auto-generated method stub
+		
+		vertex.setX(vertex.getX() + width);
+		vertex.setY(vertex.getY() + height);
+		
+		focus.setX(focus.getX() + width);
+		focus.setY(focus.getY() + height);
+		
+		if(directrixAxis.equals("y")) {
+			directrixValue += height;
+		}
+		else if(directrixAxis.equals("x")) {
+			directrixValue += width;
+		}
+	}
+
+	@Override
+	public void rotate(double angle) {
+		// TODO Auto-generated method stub
+		
+		vertex.setX(vertex.getX() * Math.cos(angle) - vertex.getY() * Math.sin(angle));
+		vertex.setY(vertex.getX() * Math.sin(angle) + vertex.getY() * Math.cos(angle));
+		
+		focus.setX(focus.getX() * Math.cos(angle) - focus.getY() * Math.sin(angle));
+		focus.setY(focus.getX() * Math.sin(angle) + focus.getY() * Math.cos(angle));
+		
+		/*
+		 * Directrix manipulation here
+		 */
+	}
+
+	@Override
+	public void reflectOverX() {
+		// TODO Auto-generated method stub
+		
+		vertex.setY(-vertex.getY());
+		focus.setY(-focus.getY());
+		
+		directrixValue = -directrixValue;
+	}
+
+	@Override
+	public void reflectOverY() {
+		// TODO Auto-generated method stub
+		
+		vertex.setX(-vertex.getX());
+		focus.setY(-vertex.getY());
+		
+		directrixValue = -directrixValue;
+	}
+
+	@Override
+	public void rescaleX(double percentage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rescaleY(double percentage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
