0c3a816695c263e30dd7f69ad08c80a820069622
Clarisse Poblete <clar.reese@gmail.com>
2015-10-24 11:01:17 +0800

    Comments (MainView)

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -8,10 +8,12 @@ import controller.Controller;
 
 public class MainView {
 
-	private static ArrayList<GraphicObject> objectList = new ArrayList<GraphicObject>();
-	private int selectedObject = 0;
+	private static ArrayList<GraphicObject> objectList;     // List of objects
+	private int 							selectedObject; // Index of selected
+
 	private JFrame mainFrame;
 	
+	// Constants
 	public static int PARABOLA_RIGHT = 1;
 	public static int PARABOLA_LEFT  = 2;
 	public static int PARABOLA_UP    = 3;
@@ -31,32 +33,39 @@ public class MainView {
 	public static int REFLECT_Y	     = 2;
 	
 	public MainView(){
-		mainFrame = new MainFrame(this);
-		
-		// Test objects
-//		objectList.add(new RectangleDrawer(-10, 5, 10, 5, false));
-//		objectList.add(new PointDrawer(-10, 5, false));
-//		objectList.add(new EllipseDrawer(0, 0, 4, 2, false));
-//		objectList.add(new LineDrawer(0, 1, 5, 6, false));
-//		objectList.add(new HyperbolaDrawer(1, 1, 5, 1, false));
+		mainFrame      = new MainFrame(this);			 // Create window
+		objectList     = new ArrayList<GraphicObject>(); // Create object list
+		selectedObject = 0;								 // Set selected object
 	}
 	
 // GETTERS
 	
+	/* Gets selected object index
+	 * @return index of selected object
+	 */
 	public int getSelectedObject(){
 		return selectedObject;
 	}
 	
+	/* Gets object
+	 * @param index index of desired object
+	 * @return object corresponding to the given index
+	 */
 	public static GraphicObject getObject(int index){
 		return objectList.get(index);
 	}
 	
+	/* Gets number of objects
+	 * @return number of objects in list
+	 */
 	public int getObjectCount(){
 		return objectList.size();
 	}
 	
 // SETTERS
 	
+	/* Selects previous object
+	*/
 	public void movePrevObject(){
 		if(selectedObject == 0){
 			selectedObject = objectList.size() - 1;
@@ -67,6 +76,8 @@ public class MainView {
 		refreshGrid();
 	}
 	
+	/* Selects next object
+	*/
 	public void moveNextObject(){
 		if(selectedObject == objectList.size() - 1){
 			selectedObject = 0;
@@ -77,34 +88,57 @@ public class MainView {
 		refreshGrid();
 	}
 	
+	/* Redraws grid and objects
+	 */
 	public void refreshGrid(){
-		System.out.println("MainView: refreshGrid");
+		System.out.println("MainView.refreshGrid()");
 		((MainFrame) mainFrame).refreshGrid();
 	}
 	
 // ADDING OBJECTS
 	
+	/* Adds point to display
+	 * @param x x coordinate of point
+	 * @param y y coordinate of point
+	 */
 	public void addPoint(double x, double y){
-		System.out.println("MainView: addPoint (" + x + ", " + y + ")");
+		System.out.println("MainView.addPoint(" + x + ", " + y + ")");
 		objectList.add(new PointDrawer(x, y, false));
 		selectedObject = objectList.size() - 1;
 		refreshGrid();
 	}
-	
+
+	/* Adds line segment to display
+	 * @param x1 x coordinate of first point
+	 * @param y1 y coordinate of first point
+	 * @param x2 x coordinate of second point
+	 * @param y2 y coordinate of second point
+	 */
 	public void addLine(double x1, double y1, double x2, double y2){
-		System.out.println("MainView: addLine (" + x1 + ", " + y1 + ", " + x2 + ", " + y2 + ")");
+		System.out.println("MainView.addLine(" + x1 + ", " + y1 + ", " + x2 + ", " + y2 + ")");
 		objectList.add(new LineDrawer(x1, y1, x2, y2, false));
 		selectedObject = objectList.size() - 1;
 		refreshGrid();
 	}
 	
+	/* Adds ellipse to display
+	 * @param x x coordinate of center
+	 * @param y y coordinate of center
+	 * @param w width
+	 * @param h height
+	 */
 	public void addEllipse(double x, double y, double w, double h){
-		System.out.println("MainView: addEllipse (" + x + ", " + y + ", " + w + ", " + h + ")");
+		System.out.println("MainView.addEllipse(" + x + ", " + y + ", " + w + ", " + h + ")");
 		objectList.add(new EllipseDrawer(x, y, w, h, false));
 		selectedObject = objectList.size() - 1;
 		refreshGrid();
 	}
 	
+	/* Adds polygon to display
+	 * @param p points defining the polygon
+	 *		  p[n][0] -> x coordinate of point n
+	 *		  p[n][1] -> y coordinate of point n
+	 */
 	public void addPolygon(double[][] p){
 		System.out.println("MainView: addPolygon");
 		objectList.add(new PolygonDrawer(p, false));
@@ -112,6 +146,15 @@ public class MainView {
 		refreshGrid();
 	}
 	
+	/* Adds parabola to display
+	 * @param x x coordinate of vertex
+	 * @param y y coordinate of vertex
+	 * @param f focal length
+	 * @param d direction of opening (MainView.PARABOLA_RIGHT
+	 *								  || MainView.PARABOLA_LEFT
+	 *								  || MainView.PARABOLA_UP
+	 *								  || MainView.PARABOLA_DOWN)
+	 */
 	public void addParabola(double x, double y, double f, int d){
 		System.out.println("MainView: addParabola (" + x + ", " + y + ", " + f + ", " + d + ")");
 		objectList.add(new ParabolaDrawer(x, y, f, d, false));
@@ -119,6 +162,14 @@ public class MainView {
 		refreshGrid();
 	}
 	
+	/* Adds parabola to display
+	 * @param x  x coordinate of vertex
+	 * @param y  y coordinate of vertex
+	 * @param xd x distance
+	 * @param yd y distance
+	 * @param d  direction of opening (MainView.HYPERBOLA_HOR
+	 *								   || HYPERBOLA_VERT)
+	 */
 	public void addHyperbola(double x, double y, double xd, double yd, int d){
 		System.out.println("MainView: addHyperbola (" + x + ", " + y + ", " + xd + ", " + yd + ", " + d + ")");
 		objectList.add(new HyperbolaDrawer(x, y, xd, yd, d, false));
@@ -128,6 +179,11 @@ public class MainView {
 	
 // UPDATING OBJECTS
 	
+	/* Updates data of given object
+	 * @param previous data of object before editing
+	 * @param index    index of object to be updated
+	 * @param data     data of edited object
+	 */
 	public void updateObject(GraphicObject previous, int index, double[][] data){
 		GraphicObject o = objectList.get(index);
 		
@@ -154,6 +210,8 @@ public class MainView {
 		refreshGrid();
 	}
 	
+	/* Removes display of object before edit
+	 */
 	public void applyEdit(){
 		objectList.remove(objectList.size() - 1);
 		refreshGrid();
@@ -161,6 +219,9 @@ public class MainView {
 	
 // DELETING OBJECTS
 	
+	/* Deletes an object
+	 * @param index index of object to delete
+	 */
 	public void deleteObject(int index){
 		objectList.remove(index);
 		if(selectedObject == index){
@@ -176,6 +237,10 @@ public class MainView {
 	
 // GETTING CONSTANT VALUES
 	
+	/* Returns string describing the parabola direction given
+	 * @param d integer representing the direction
+	 * @return string describing direction
+	 */
 	public static String getPDirectionString(int d){
 		if(d == PARABOLA_RIGHT){
 			return "Opening Right";
@@ -194,6 +259,10 @@ public class MainView {
 		}
 	}
 	
+	/* Returns string describing the hyperbola direction given
+	 * d integer representing the direction
+	 * @return string describing direction
+	 */
 	public static String getHDirectionString(int d){
 		if(d == HYPERBOLA_HOR){
 			return "Opening Right & Left";
